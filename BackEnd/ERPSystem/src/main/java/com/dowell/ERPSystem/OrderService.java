1732e43a44baa1f82bbeb354b13296dfe5a9f822
unknown <samljin123@gmail.com>
2024-12-02 10:07:35 +0800

    前端檢查完成，後端service層新增@transactional，讓訂單新增失敗時會rollback

--- BackEnd/ERPSystem/src/main/java/com/dowell/ERPSystem/OrderService.java
+++ BackEnd/ERPSystem/src/main/java/com/dowell/ERPSystem/OrderService.java
@@ -3,18 +3,17 @@ package com.dowell.ERPSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import jakarta.transaction.Transactional;
+
 @Component
 public class OrderService {
 	//用來處理order與資料庫連線的變數
 	@Autowired
 	private OrderDao orderDao;
 	
+	@Transactional
 	//新增訂單
 	public Order insert_Order(Order order) {
 		return orderDao.insert_Order(order);
-	}
-	//新增訂單品項
-	public OrderItem insert_OrderItem(OrderItem orderItem) {
-		return orderDao.insert_OrderItem(orderItem);
 	}
 }
